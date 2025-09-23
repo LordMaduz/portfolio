f6ba226f71b8d429582e93e087abafbca6ecac18
Ruchira Madhushan Rajapaksha <52396694+LordMaduz@users.noreply.github.com>
2025-09-23 21:07:34 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/ruchira-portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
