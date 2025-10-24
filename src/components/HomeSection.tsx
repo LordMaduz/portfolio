0d6f810f691f8b757c4eeb1045c63f1653b42647
LordMaduz <maduz.ruchira@gmail.com>
2025-10-25 01:09:19 +0800

    Home section data change.

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -100,7 +100,7 @@ const scrollToSection = (sectionId: string) => {
               </div>
 
               <div className="space-y-2 border-l-2 border-lime-400 pl-4">
-                <div className="text-3xl font-bold text-lime-400">5</div>
+                <div className="text-3xl font-bold text-lime-400">10+</div>
                 <div className="text-md text-gray-400 leading-tight">
                   Enterprise
                   <br />
